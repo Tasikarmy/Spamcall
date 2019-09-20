eabc4e596ffccc2f0c6ed1fcf6cc7990cd1024e6
Tasikarmy <55316231+Tasikarmy@users.noreply.github.com>
2019-09-20 16:18:35 +0700

    Add files via upload

---
+++ InstallDuluCoeg.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+echo -e $blue "Bismillah dulu Gans"
+echo -e $cyan "╯▅╰╱▔▔▔▔▔▔▔╲╯╯☼"
+echo -e $cyan "▕▕╱╱╱╱╱╱╱╱╱╲╲╭╭"
+echo -e $cyan "▕▕╱╱╱╱╱╱╱╱┛▂╲╲╭"
+echo -e $cyan "╱▂▂▂▂▂▂╱╱┏▕╋▏╲╲"
+sleep 1
+echo -e $cyan "▔▏▂┗┓▂▕▔┛▂┏▔▂▕▔"
+echo -e $cyan "▕▕╋▏▕╋▏▏▕┏▏▕╋▏▏"
+echo -e $cyan "▕┓▔┗┓▔┏▏▕┗▏┓▔┏▏"
+echo -e $red  "Waktunya ngopi"
+sleep 2
+echo -e $m    "▄▀▀ ▄▀▄ █▄░█ ▀█▀ █░█ ▀▄░▄▀"
+echo -e $m    "░▀▄ █▀█ █░▀█ ░█░ █░█ ░░█░░"
+echo -e $m    "▀▀░ ▀░▀ ▀░░▀ ░▀░ ░▀░ ░░▀░░"
+figlet install | lolcat
+sleep 4
+echo -e $bu " Menginstal..."
+pkg install php
+pkg install wget
+pkg install figlet
+pkg install toilet
+pkg install python
+pkg install ruby
+PKG install wget openssl-tool proot -y
+gem install lolcat
+pip2 install requests
+pip2 install mechanize
+pip2 install tqdm
+pip2 install termcolor
+clear
+echo -e $red " Sedang Masuk Tols"
+sleep 4
+php Spammer.php
